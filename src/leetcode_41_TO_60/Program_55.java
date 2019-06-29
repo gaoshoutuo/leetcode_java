f62ed460f52c87feb671e33616e990f86cdd45df
zth <715686280@qq.com>
2019-06-29 22:54:29 +0800

    leetcode 50

--- src/leetcode_41_TO_60/Program_55.java
+++ src/leetcode_41_TO_60/Program_55.java
@@ -1,6 +1,7 @@
 package leetcode_41_TO_60;
 
-public class Program_55 {//Jump Game  给出数组 是否能够跳到最后 imdex
+public class Program_55 {//Jump Game  给出数组 是否能够跳到最后 imdex  恩我的技术敏感度数有点下降了 所以要分配时间不能全
+    //刷题 也要学新技术 查漏补缺 基本就是 flutter spring 英语 文章 语言
     static class Solution {
         public boolean canJump(int[] nums) {
             return false;
