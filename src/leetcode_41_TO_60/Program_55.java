a686d6a6532f1389629db9d4214fd945b7c76097
zth <715686280@qq.com>
2019-06-29 23:21:34 +0800

    leetcode 50

--- src/leetcode_41_TO_60/Program_55.java
+++ src/leetcode_41_TO_60/Program_55.java
@@ -1,7 +1,8 @@
 package leetcode_41_TO_60;
 
 public class Program_55 {//Jump Game  给出数组 是否能够跳到最后 imdex  恩我的技术敏感度数有点下降了 所以要分配时间不能全
-    //刷题 也要学新技术 查漏补缺 基本就是 flutter spring 英语 文章 语言
+    //刷题 也要学新技术 查漏补缺 基本就是 flutter spring 英语 文章 语言  乐谱  有没有什么办法一写出就能够知道自己对不对
+    //提前做好各类打算 先从找工作开始  积极乐观的心态 未雨绸缪的打算  做出成果 做在实处
     static class Solution {
         public boolean canJump(int[] nums) {
             return false;
