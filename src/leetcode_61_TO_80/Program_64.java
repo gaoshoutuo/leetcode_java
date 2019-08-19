f97d9accc6a9d784dfd8f656a6cac24ad0ab8824
zth <715686280@qq.com>
2019-08-19 20:19:27 +0800

    一小时刷3道leetcode 2 easy 1medium

--- src/leetcode_61_TO_80/Program_64.java
+++ src/leetcode_61_TO_80/Program_64.java
@@ -12,9 +12,34 @@ Output: 7
 Explanation: Because the path 1→3→1→1→1 minimizes the sum.
  */
 
-    static class Solution {
+    public static void main(String []args){
+        Solution solution=new Solution();
+        System.out.println(solution.minPathSum(new int[][]{{1,3,1},{1,5,1},{4,2,1}}));
+
+    }
+
+    static class Solution {//思路就是开一个新的数组然后做累加 每个cell填写左上最小累加 输出最后之n-1 n-1
         public int minPathSum(int[][] grid) {
-            return 0;
+            //int [][]mat=new int[grid.length][grid[0].length];
+            for (int i=0;i<grid.length;i++){
+                for (int j=0;j<grid[0].length;j++){
+                   if (i==0&&j==0){
+                       grid[i][j]=grid[i][j];
+                   }else if (i==0){
+                       grid[i][j]= grid[i][j]+ grid[i][j-1];
+                   }else if (j==0){
+                       grid[i][j]= grid[i-1][j]+ grid[i][j];
+                   }else{
+                       grid[i][j]=Math.min(grid[i-1][j]+grid[i][j],grid[i][j]+grid[i][j-1]);
+                   }
+                }
+            }
+            return grid[grid.length-1][grid[0].length-1];
         }
     }
+    /**
+     * Runtime: 2 ms, faster than 90.21% of Java online submissions for Minimum Path Sum.
+     * Memory Usage: 42.3 MB, less than 83.78% of Java online submissions for Minimum Path Sum.
+     * 只能说这是速战速决 非常快速的刷题
+     */
 }
--- src/leetcode_61_TO_80/Program_70.java
+++ src/leetcode_61_TO_80/Program_70.java
@@ -9,4 +9,43 @@ Explanation: There are two ways to climb to the top.
 1. 1 step + 1 step
 2. 2 steps
      */
+
+    public static void main(String []args){
+        Solution solution=new Solution();
+        System.out.println(solution.climbStairs(5));
+    }
+
+    static class Solution {// 纯粹自己写的 会不会超时啊 a few seconds ago	Time Limit Exceeded	N/A	N/A	java
+        // 发现了这就是斐波那契数列 要叠算  22，33，45，58，613  就相当与第六层是第五层+第4层 这种意思
+        /*int p=0;
+        public int climbStairs(int n) {
+            reI(n);
+            return p;
+        }
+        public void reI(int n){
+            if (n>0){
+                reI(n-1);
+                reI(n-2);
+            }else if (n==0){
+                p++;
+                return;
+            }
+        }*/
+        public int climbStairs(int n) {
+            if(n <= 0) return 0;
+            if(n == 1) return 1;
+            if(n == 2) return 2;
+            int a1=0,a2=1,a3=2;
+            for(int i=2;i<n;i++){
+                a1=a2+a3;
+                a2=a3;
+                a3=a1;
+            }
+            return a1;
+        }
+        /**
+         * Runtime: 0 ms, faster than 100.00% of Java online submissions for Climbing Stairs.
+         * Memory Usage: 32.9 MB, less than 5.26% of Java online submissions for Climbing Stairs.
+         */
+    }
 }
